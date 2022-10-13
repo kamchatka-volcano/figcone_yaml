78c4eb7b5fb0ef2fabedff454741a313aad3b41f
kamchatka_volcano <gorelyy@vivaldi.net>
2022-10-14 00:42:40 +0300

    -changed library type to object library

--- include/figcone_yaml/yaml_impl.h
+++
@@ -1,7 +0,0 @@
-#ifndef FIGCONE_YAML_YAML_IMPL_H
-#define FIGCONE_YAML_YAML_IMPL_H
-
-#define RYML_SINGLE_HDR_DEFINE_NOW
-#include "detail/external/ryml_all.hpp"
-
-#endif //FIGCONE_YAML_YAML_IMPL_H
